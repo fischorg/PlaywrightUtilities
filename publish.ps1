b3dc77a71b17248f61295e60a0a182e11b1ac414
Paul Staszko <paulstaszko@gmail.com>
2024-07-17 20:40:20 -0500

    quick

--- publish.ps1
+++ publish.ps1
@@ -1,25 +1,25 @@
 Param(
   [switch]$SkipDuplicationCheck
 )
-pushDir
+Push-Location
 cd $PSScriptRoot
-paket install
-if($LASTEXITCODE){
-    Write-Error "Failed to install paket dependencies"
-}else{
-    dotnet pack ./PlaywrightUtilities --include-source
+dotnet pack --include-source
+if($lastexitcode -eq 0){
     $f=dir *.nupkg -rec | sort LastWriteTime -desc | select -first 1
-    ## NOTE: The WCRI_GITHUB_PACKAGES_PASSWORD is a Github classic personal access token with package write priivleges
-    $url = "https://nuget.pkg.github.com/WesternCapital/index.json"
+    ## NOTE: The PUBLIC_GITHUB_PACKAGES_PASSWORD is a Github classic personal access token with package write priivleges
+    $f
+    $url = "https://nuget.pkg.github.com/fischorg/index.json"
     if($SkipDuplicationCheck){
-        dotnet nuget push $f --source $url --api-key $env:WCRI_GITHUB_PACKAGES_PASSWORD --skip-duplicate
+        dotnet nuget push $f --source $url --api-key $env:PUBLIC_GITHUB_PACKAGES_PASSWORD --skip-duplicate
     }else{
-        dotnet nuget push $f --source $url --api-key $env:WCRI_GITHUB_PACKAGES_PASSWORD
+        dotnet nuget push $f --source $url --api-key $env:PUBLIC_GITHUB_PACKAGES_PASSWORD
     }
     if($LASTEXITCODE -eq 0){
         Write-Host "Successfully pushed package to Github Packages"
     }else{
         Write-Error "Failed to push package to Github Packages: $LASTEXITCODE"
     }
+}else{
+    Write-Error "Failed to pack package: $LASTEXITCODE"
 }
-popdir
+Pop-Location
